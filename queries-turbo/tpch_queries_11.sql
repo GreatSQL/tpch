aa290a9e9cc79de52105f9839c3a67ccd7a4bd41
Ye Jinrong <jinrong.ye@greatdb.com>
2025-02-17 16:47:21 +0800

    TPC-H 3.0.1 for GreatSQL, init

---
+++ queries-turbo/tpch_queries_11.sql
@@ -0,0 +1,30 @@
+SELECT /*+ SET_VAR(turbo_enable=ON) SET_VAR(turbo_cost_threshold=0) */ /*+ Q11 */
+    ps_partkey,
+    sum(ps_supplycost * ps_availqty) AS value
+FROM
+    partsupp,
+    supplier,
+    nation
+WHERE
+    ps_suppkey = s_suppkey
+    AND s_nationkey = n_nationkey
+    AND n_name = 'GERMANY'
+GROUP BY
+    ps_partkey
+HAVING
+    sum(ps_supplycost * ps_availqty) > (
+        SELECT
+            sum(ps_supplycost * ps_availqty) * 0.0001000000 /* SF1 */
+            /* sum(ps_supplycost * ps_availqty) * 0.0000100000 /* SF10 */
+            /* sum(ps_supplycost * ps_availqty) * 0.0000010000 /* SF100 */
+            /* sum(ps_supplycost * ps_availqty) * 0.0000001000 /* SF1000 */
+        FROM
+            partsupp,
+            supplier,
+            nation
+        WHERE
+            ps_suppkey = s_suppkey
+            AND s_nationkey = n_nationkey
+            AND n_name = 'GERMANY')
+ORDER BY
+    value DESC;
---
+++ queries-turbo/tpch_queries_17.sql
@@ -0,0 +1,16 @@
+SELECT /*+ SET_VAR(turbo_enable=ON) SET_VAR(turbo_cost_threshold=0) */ /*+ Q17 */
+    sum(l_extendedprice) / 7.0 AS avg_yearly
+FROM
+    lineitem,
+    part
+WHERE
+    p_partkey = l_partkey
+    AND p_brand = 'Brand#23'
+    AND p_container = 'MED BOX'
+    AND l_quantity < (
+        SELECT
+            0.2 * avg(l_quantity)
+        FROM
+            lineitem
+        WHERE
+            l_partkey = p_partkey);
